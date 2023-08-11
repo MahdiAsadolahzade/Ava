a45584fb2a8a9f2eae3cab4e28ad5b752a927c32
Mahdi Asadolahzade <mahdiasadi140@gmail.com>
2023-08-11 23:33:25 +0330

    Components Update V8

--- src/Components/ArchiveTable.tsx
+++ src/Components/ArchiveTable.tsx
@@ -2,7 +2,7 @@ import ArchiveSideButtons from "../assets/Icons/ArchiveSideButtons";
 
 function ArchiveTable() {
   return (
-    <table className="table-auto mx-auto">
+    <table className="table-auto mx-auto mt-[20px] w-[80%]">
       <thead>
         <tr>
           <th></th>
@@ -14,36 +14,24 @@ function ArchiveTable() {
         </tr>
       </thead>
       <tbody>
-        <tr className="w-[100%]">
-          <td className="w-[10%]">{ArchiveSideButtons.Link}</td>
-          <td className="w-[40%] ">
-            <p className="truncate w-[100%] text-sky-600">
-              https://irsv.upmusics.com/Downloads/Musick
+        <tr className="justify-around">
+          <td className="w-[5%]">{ArchiveSideButtons.Link}</td>
+          <td className=" w-[40%]">
+            <p className="truncate w-[435px] text-sky-600">
+            https://irsv.upmusics.com/Downloads/Musics/Sirvan%20Ky637fgbndrt94
             </p>
           </td>
-          <td className="w-[10%]">۱۴۰۰-۰۸-۲۱</td>
+          <td className="w-[15%]">۱۴۰۰-۰۸-۲۱</td>
           <td className="w-[10%]">.mp3</td>
           <td className="w-[10%]">۴:۲۹</td>
-          <td className="flex flex-row justify-around w-[20%]">
-            <div>{ArchiveSideButtons.Download}</div>
-            <div>{ArchiveSideButtons.Word}</div>
-            <div>{ArchiveSideButtons.Copy}</div>
-            <div>{ArchiveSideButtons.Delete}</div>
+          <td className="flex flex-row justify-evenly items-center ">
+            <div className="cursor-pointer ">{ArchiveSideButtons.Download}</div>
+            <div className="cursor-pointer ">{ArchiveSideButtons.Word}</div>
+            <div className="cursor-pointer " >{ArchiveSideButtons.Copy}</div>
+            <div className="cursor-pointer hover:bg-red-500 hover:rounded-full">{ArchiveSideButtons.Delete}</div>
           </td>
         </tr>
-        <tr>
-          <td>{ArchiveSideButtons.Upload}</td>
-          <td className="w-[400px] flex flex-row">
-            <p className="truncate w-[90%]">khaterate To</p>
-          </td>
-          <td>The Eagles</td>
-          <td>1972</td>
-        </tr>
-        <tr>
-          <td>Shining Star</td>
-          <td>Earth, Wind, and Fire</td>
-          <td>1975</td>
-        </tr>
+
       </tbody>
     </table>
   );
