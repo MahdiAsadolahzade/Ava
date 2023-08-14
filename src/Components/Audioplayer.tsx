db666adae7562e85fb0793750d0c21d0d42495f7
Mahdi Asadolahzade <mahdiasadi140@gmail.com>
2023-08-14 10:49:07 +0330

    Component of Audio Player Updated

--- src/Components/Audioplayer.tsx
+++ src/Components/Audioplayer.tsx
@@ -4,40 +4,113 @@ import Playicon from "../assets/Icons/Playicon";
 import Stopicon from "../assets/Icons/Stopicon";
 import Volumebaricon from "../assets/Icons/Volumebaricon";
 
-function Audioplayer() {
+import React, { useRef, useState, useEffect } from "react";
+
+const AudioPlayer: React.FC = () => {
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [thumbPosition, setThumbPosition] = useState(0);
+  const [loadedProgress, setLoadedProgress] = useState(0);
+
+  const togglePlay = () => {
+    if (audioRef.current?.paused) {
+      audioRef.current?.play();
+      setIsPlaying(true);
+    } else {
+      audioRef.current?.pause();
+      setIsPlaying(false);
+    }
+  };
+
+  const handleInputRangeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newPosition = parseFloat(e.target.value);
+    setThumbPosition(newPosition);
+
+    if (audioRef.current) {
+      audioRef.current.currentTime =
+        (newPosition / 100) * audioRef.current.duration;
+    }
+  };
+
+  const handleStop = () => {
+    if (audioRef.current) {
+      audioRef.current.pause();
+      audioRef.current.currentTime = 0;
+      setIsPlaying(false);
+    }
+  };
+
+  useEffect(() => {
+    const progressInterval = setInterval(() => {
+      if (audioRef.current) {
+        const currentTime = audioRef.current.currentTime;
+        const totalDuration = audioRef.current.duration;
+        const newThumbPosition = (currentTime / totalDuration) * 100;
+        setThumbPosition(newThumbPosition);
+
+        if (audioRef.current.buffered.length > 0) {
+          const loadedTime = audioRef.current.buffered.end(0);
+          const newLoadedProgress = (loadedTime / totalDuration) * 100;
+          setLoadedProgress(newLoadedProgress);
+        }
+      }
+    }, 100);
+
+    return () => clearInterval(progressInterval);
+  }, [audioRef]);
+
   return (
-    <div
-      className="Container bg-[#F8F8F8] rounded-lg flex flex-row items-center justify-center w-[80%] mx-auto h-8"
-      dir="ltr"
-    >
-      <div className="ml-[10px]">
+    <div className="flex flex-row justify-center items-center" dir="ltr">
+      <audio ref={audioRef} src={Example} />
+      <div className="ml-[10px] cursor-pointer"
+      onClick={handleStop}>
         <Stopicon></Stopicon>
       </div>
-      <div className="mx-[10px]">
-        <Pauseicon></Pauseicon>
+      <div className="px-[10px] cursor-pointer" onClick={togglePlay}>
+        {isPlaying ? <Pauseicon /> : <Playicon />}
       </div>
-      
+      <div className="play w-full flex flex-row items-center relative">
+        <input
+          type="range"
+          className="w-full h-px thumb"
+          min="0"
+          max="100"
+          step="0.01"
+          value={thumbPosition}
+          onChange={handleInputRangeChange}
+          style={{
+            appearance: "none",
+            cursor: "pointer",
+            zIndex: 2,
+            outline: "none",
+            border: "none",
+            width: "100%",
+          }}
+        />
+        <div
+          className="w-full h-px border border-[#C6C6C6]"
+          style={{
+            position: "absolute",
+            top: "50%",
+            transform: "translateY(-50%)",
+            zIndex: 0,
+          }}
+        ></div>
+        <div
+          className="progress-bar w-full h-px border-1 border-[#898989] absolute"
+          style={{ width: `${loadedProgress}%`, zIndex: 1 }}
+        ></div>
+        <div
+          className="played w-full h-px border-1 border-sky-600 absolute"
+          style={{ width: `${thumbPosition}%`, zIndex: 3 }}
+        ></div>
+      </div>
 
-      <div className="play w-[100%] flex flex-row relative">
-        <div className="w-[100%]  h-px border border-[#C6C6C6] absolute"></div>
-        <div className="w-[70%] h-px border-1 border-[#898989] absolute"></div>
-        <div className="w-[40%] h-px border-1 border-sky-600 absolute"></div>
-        <div className="w-3.5 h-3.5 left-[40%] top-[-7px] bg-sky-600 rounded-full border border-sky-600 absolute"></div>
-      </div>
-
-      <div className="mx-[10px] ">
-        ۴:۲۹
-      </div>
-
-      <div className="mx-[5px]">
-        <Volumebaricon></Volumebaricon>
-      </div>
-      <div className="volume w-[20%] flex flex-row relative">
-        <div className="w-[80%]  h-px border border-[#C6C6C6] absolute"></div>
-        <div className="w-[40%] h-px border-1 border-sky-600 absolute"></div>
+      <div>
+        
       </div>
     </div>
   );
-}
+};
 
-export default Audioplayer;
+export default AudioPlayer;
