c63b61c00bc546929af51f4dd430afc64aacc92c
Mahdi Asadolahzade <mahdiasadi140@gmail.com>
2023-08-22 17:05:39 +0330

    Deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Goftarpage from "./Pages/Goftarpage";
 import Archivepage from "./Pages/Archivepage";
 
 function App() {
+  console.log("hi");
   return (
     <>
       <Router 
-      basename={import.meta.env.DEV ? '/' : '/Ava/'}
+      // basename={import.meta.env.DEV ? '/' : '/Ava/'}
       >
         <Routes>
+        <Route path="/" element={<Navigate to="/Ava/" />} />
           <Route path="/Ava/" element={<Goftarpage />} />
           <Route path="/Ava/archive/" element={<Archivepage />} />
         </Routes>
